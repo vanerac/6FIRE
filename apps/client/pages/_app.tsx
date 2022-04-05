b3b893b383649282dfbffb1d01dec4c912ab3796
alexquenelle <alexandre.quenelle@epitech.eu>
2022-04-05 21:19:52 +0200

    client/test_responsive [ADD] beta responsive register page

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import '../css/register-web.css';
+import '../css/styleguide.css';
+import '../css/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
