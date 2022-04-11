8223b140184d86f5097ea9c531cf21e3094ff012
Corentin MAS <corentinmas@yahoo.com>
2022-04-11 11:58:00 +0200

    [feature/subscription-service] Removed comments

--- apps/api/src/entities/subscription/subscription.controller.ts
+++ apps/api/src/entities/subscription/subscription.controller.ts
@@ -4,48 +4,6 @@ import { CRUDController } from '../../types';
 
 const prisma = new PrismaClient();
 
-// model Subscription {
-//   id               Int                @id @default(autoincrement())
-//   name             String
-//   description      String
-//   price            Int
-//   level            Int                @default(0)
-//   createdAt        DateTime           @default(now())
-//   updatedAt        DateTime           @default(now())
-//   UserSubscription UserSubscription[]
-// }
-//
-// model UserSubscription {
-//   id             Int          @id @default(autoincrement())
-//   userId         Int
-//   subscriptionId Int
-//   createdAt      DateTime     @default(now())
-//   updatedAt      DateTime     @default(now())
-//   User           User         @relation(fields: [userId], references: [id])
-//   Subscription   Subscription @relation(fields: [subscriptionId], references: [id])
-// }
-//
-// model User {
-//   id                Int                 @id @default(autoincrement())
-//   userId            String              @unique @default(uuid())
-//   firstName         String
-//   lastName          String
-//   email             String
-//   password          String
-//   telephone         String
-//   countryId         Int
-//   createdAt         DateTime            @default(now())
-//   updatedAt         DateTime            @default(now())
-//   isAdmin           Boolean             @default(false)
-//   verifiedEmail     Boolean             @default(false)
-//   verifiedPhone     Boolean             @default(false)
-//   banned            Boolean             @default(false)
-//   country           Country             @relation(fields: [countryId], references: [id])
-//   VerificationCodes VerificationCodes[]
-//   TraderFollows     TraderFollows[]
-//   UserSubscription  UserSubscription[]
-// }
-
 export default class SubscriptionController extends CRUDController {
     static async getAll(req: Request, res: Response) {
         try {
