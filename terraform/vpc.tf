394f1a3a73cd699854944003421b79825468bafa
Corentin MAS <corentinmas@yahoo.com>
2022-04-13 21:54:25 +0200

    [main] Commented VPC creation

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,24 +4,24 @@
 // one for database
 // one for load balancer
 
-resource "aws_vpc" "api" {
-  cidr_block = "10.0.0.0/28"
-}
-
-resource "aws_vpc" "dashboard" {
-  cidr_block = "10.0.0.0/28"
-}
-
-
-resource "aws_vpc" "client" {
-  cidr_block = "10.0.0.0/28"
-}
-
-resource "aws_vpc" "database" {
-  cidr_block = "10.0.0.0/28"
-}
-
-resource "aws_vpc" "loadbalancer" {
-  cidr_block = "10.0.0.0/28"
-
-}
+#resource "aws_vpc" "api" {
+#  cidr_block = "10.0.0.0/28"
+#}
+#
+#resource "aws_vpc" "dashboard" {
+#  cidr_block = "10.0.0.0/28"
+#}
+#
+#
+#resource "aws_vpc" "client" {
+#  cidr_block = "10.0.0.0/28"
+#}
+#
+#resource "aws_vpc" "database" {
+#  cidr_block = "10.0.0.0/28"
+#}
+#
+#resource "aws_vpc" "loadbalancer" {
+#  cidr_block = "10.0.0.0/28"
+#
+#}
