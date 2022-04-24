46b9130e63b1d702ec26227ccbf4a28228304960
Corentin MAS <corentinmas@yahoo.com>
2022-04-24 14:28:27 +0200

    [feature/deployment-linking] Implemented ApiClient to use baseUrl for sdk requests

---
+++ shared/tools/apiClient.ts
@@ -0,0 +1,11 @@
+import Cookies from 'universal-cookie';
+import {ApiClient} from "../services";
+
+export default function getApiClient(host: string = process.env.NEXT_PUBLIC_API_ROUTE) {
+    const cookies = new Cookies();
+    cookies.get('API_TOKEN');
+    return new ApiClient({
+        TOKEN: cookies.get('API_TOKEN'),
+        BASE: host || 'http://localhost:8080'
+    });
+}
