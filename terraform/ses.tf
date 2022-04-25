c6c8cec1c38d66f687a1a895812e03f88a90f555
Corentin MAS <corentinmas@yahoo.com>
2022-04-25 21:12:16 +0200

    [feature/ses-setup] Added commented SES terraform setup

--- terraform/ses.tf
+++ terraform/ses.tf
@@ -1,28 +1,40 @@
-// AWS SES
-#resource "aws_ses_domain_mail_from" "example" {
-#  domain           = aws_ses_domain_identity.example.domain
-#  mail_from_domain = "bounce.${aws_ses_domain_identity.example.domain}"
+#data "aws_region" "current" {}
+#data "aws_caller_identity" "current" {}
+#
+#
+#data "aws_route53_zone" "zone" {
+#  name = "6fireinvest.com"
+#  tags = {
+#    project = "6fire"
+#  }
 #}
 #
-## Example SES Domain Identity
-#resource "aws_ses_domain_identity" "example" {
-#  domain = "example.com"
+#resource "aws_ses_domain_identity" "primary" {
+#  domain = data.aws_route53_zone.zone.name
 #}
 #
-## Example Route53 MX record
-#resource "aws_route53_record" "example_ses_domain_mail_from_mx" {
-#  zone_id = aws_route53_zone.example.id
-#  name    = aws_ses_domain_mail_from.example.mail_from_domain
-#  type    = "MX"
-#  ttl     = "600"
-#  records = ["10 feedback-smtp.us-east-1.amazonses.com"] # Change to the region in which `aws_ses_domain_identity.example` is created
-#}
-#
-## Example Route53 TXT record for SPF
-#resource "aws_route53_record" "example_ses_domain_mail_from_txt" {
-#  zone_id = aws_route53_zone.example.id
-#  name    = aws_ses_domain_mail_from.example.mail_from_domain
+#resource "aws_route53_record" "ses_verif" {
+#  zone_id = data.aws_route53_zone.zone.zone_id
+#  name    = "_amazonses.${aws_ses_domain_identity.primary.id}"
 #  type    = "TXT"
 #  ttl     = "600"
-#  records = ["v=spf1 include:amazonses.com -all"]
+#  records = [aws_ses_domain_identity.primary.verification_token]
+#}
+#
+#resource "aws_ses_domain_identity_verification" "ses_verif" {
+#  domain = aws_ses_domain_identity.primary.id
+#
+#  depends_on = [aws_route53_record.ses_verif]
+#}
+#
+#resource "aws_route53_record" "email" {
+#  zone_id = data.aws_route53_zone.zone.zone_id
+#  name    = data.aws_route53_zone.zone.name
+#  type    = "MX"
+#  ttl     = "600"
+#  records = ["10 inbound-smtp.${data.aws_region.current.name}.amazonaws.com"]
+#}
+#
+#resource "aws_ses_email_identity" "email" {
+#  email = "no-reply@6fireinvest.com"
 #}
