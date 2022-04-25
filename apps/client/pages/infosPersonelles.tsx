fbacbac647eb0c3e91936b0110cae8eb9d41c7a3
Zihad Hosan <zihadhossain88@hotmail.com>
2022-04-26 02:57:16 +0600

    /infosPersonelles markup/style and responsive

--- apps/client/pages/infosPersonelles.tsx
+++ apps/client/pages/infosPersonelles.tsx
@@ -17,11 +17,41 @@ const Compte: NextPage = (props: any) => {
     }, []);
 
     return (
-        <>
+        <div>
             <Header isOpenSideBar={props.useStateOpenSideBar} isEspaceTradingCrypto={false} />
-            personnal informations page
+                <div className="account-wrapper">
+                    <div className="breadcurm">
+                        <span>Compte</span>
+                        <img src="/img/info-icon/right-arrow.png" alt="" />
+                        <span>Informations personnelles</span>
+                    </div>
+                    <div className="main-title">
+                        <h1 className="title">Informations personnelles<span>Modifiez vos données personnelles</span></h1>
+                    </div>
+
+                    <div className="infor-form">
+                        <form action="#">
+                            <div className="input-wrap">
+                                <input type="text" placeholder='* Nom' />
+                                <input type="text" placeholder='* Prénom' />
+                                <input type="email" placeholder='* Email' />
+                                <input type="tel" placeholder='* Numéro de téléphone' />
+                            </div>
+
+                            <div className="send_btn">
+                                <button type='submit' className="primary-button">
+                                    <span>Modifier</span>
+                                    <div className="right-arrow">
+                                        <img src="/img/icon/right-arrow.png" alt="" />
+                                    </div>
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
             <Footer />
-        </>
+        </div>
     );
 };
 
