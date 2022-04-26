7069c5aba27a5389745ab2607484cd84578b77c2
Corentin MAS <corentinmas@yahoo.com>
2022-04-26 10:55:31 +0200

    [feature/bot-message-queue] Added message queue

--- apps/bot/src/cache.ts
+++ apps/bot/src/cache.ts
@@ -1,11 +1,13 @@
 import { Position } from '@shared/scraper/types';
 import { createClient } from 'redis';
 import configuration from '../configuration';
+import EventEmitter from 'events';
 
-export default class Cache {
+export default class Cache extends EventEmitter {
     private client;
 
     constructor() {
+        super();
         this.client = createClient({ url: configuration.REDIS_URL });
     }
 
@@ -15,5 +17,15 @@ export default class Cache {
 
     async setTrader(traderId: string, positions: Position[]) {
         await this.client.hSet(traderId, 'positions', positions);
+    }
+
+    async addMessage(chatId: string, message: string) {
+        this.emit('message_add');
+        console.log('Message Added to queue');
+        await this.client.rpush('pending_messages', { message, chatId });
+    }
+
+    async getMessage(): Promise<{ chatId: string; message: string }> {
+        return await this.client.lpop('pending_messages');
     }
 }
--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import Database from './database';
 import Cache from './cache';
 import { ScrapeDifferential, scrapeDifferential } from './calc';
-import sendMessage from './bot';
+import TelegramBot from './bot';
 
 dotenv.config();
 /* Todo:
@@ -12,37 +12,46 @@ dotenv.config();
  */
 
 const cache = new Cache();
+const bot = new TelegramBot();
+const scraper = Scraper.getInstance(process.env.APIFY_KEY);
 
 async function formatMessage(traderId: string, data: ScrapeDifferential) {
     return `${traderId} has opened ${data.opened.length} and closed ${data.closed.length} positions.`;
 }
 
-(async () => {
-    const scraper = Scraper.getInstance(process.env.APIFY_KEY);
+bot.listenMessageQueue();
+
+async function RunBot() {
     const curatedTraders = await Database.getCuratedTraders();
-    const result = await Promise.all(
+    await Promise.all(
         curatedTraders.map(async (trader) => {
             // Todo: Note: can i use the array of profiles to accelerate scrapes ?
-            // Todo: Note: This also mitigates the flood of requests to the API when sending a message
+            // Todo: Note: On the other side, this also mitigates the flood of requests to the API when sending a message
             const data = await scraper.scrapeProfile([trader.id]);
             const { positions } = data;
             console.log(`${name} has a position of ${positions}`);
             const previousPositions = await cache.getPositions(trader.id);
             await cache.setTrader(trader.id, positions);
-            return { traderId: trader.id, data: scrapeDifferential(previousPositions, positions) };
+            const res = { traderId: trader.id, data: scrapeDifferential(previousPositions, positions) };
 
             // Todo send notification here ?
+
+            const followers = await Database.getFollowers(res.traderId); // Todo: Note: can i use the cache for this instead
+            followers.map(async (follower) => {
+                const formattedMessage = await formatMessage(res.traderId, res.data);
+                cache.addMessage(follower.User.telegramId, formattedMessage);
+            });
         }),
     );
-    console.log(result);
+}
 
-    // Todo: setup a message queue with redis, telegram wont like 500 message in a row
-
-    result.map(async (res) => {
-        const followers = await Database.getFollowers(res.traderId);
-        followers.map(async (follower) => {
-            const formattedMessage = await formatMessage(res.traderId, res.data);
-            await sendMessage(follower.User.telegramId, formattedMessage);
-        });
-    });
+(async () => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        const now = new Date();
+        await RunBot();
+        const timeToWait = new Date(now.getTime() + 1000 * 60 * 5); // 5m
+        console.log(`Waiting for ${timeToWait}`);
+        await new Promise((resolve) => setTimeout(resolve, timeToWait.getTime() - now.getTime()));
+    }
 })();
