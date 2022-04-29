036d63b217bfc92534d7de8c870a7884cbb4b615
Zihad Hosan <zihadhossain88@hotmail.com>
2022-04-30 00:56:33 +0600

    Mobile overlay fixing

--- apps/dashboard/components/sidebarNew.tsx
+++ apps/dashboard/components/sidebarNew.tsx
@@ -1,4 +1,31 @@
 import React from 'react';
+import $ from 'jquery';
+import style from '../styles/main.module.scss';
+
+
+if (typeof window !== "undefined"){
+    $(".sidebar-dropdown > a").click(function() {
+        $(".sidebar-submenu").slideUp(200);
+        if (
+          $(this)
+            .parent()
+            .hasClass("active")
+        ) {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this)
+            .parent()
+            .removeClass("active");
+        } else {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this)
+            .next(".sidebar-submenu")
+            .slideDown(200);
+          $(this)
+            .parent()
+            .addClass("active");
+        }
+      });
+}
 
 export default function sideBar() {
     // Todo: Add search functionally
@@ -12,28 +39,8 @@ export default function sideBar() {
                     <ul>
                     <li className="sidebar-dropdown">
                         <a href="#">
-                        <i className="fa fa-tachometer-alt"></i>
-                        </a>
-                        <div className="sidebar-submenu">
-                        <ul>
-                            <li>
-                            <a href="#">Dashboard 1
-                                <span className="badge badge-pill badge-success">Pro</span>
-                            </a>
-                            </li>
-                            <li>
-                            <a href="#">Dashboard 2</a>
-                            </li>
-                            <li>
-                            <a href="#">Dashboard 3</a>
-                            </li>
-                        </ul>
-                        </div>
-                    </li>
-                    <li className="sidebar-dropdown">
-                        <a href="#">
                         <i className="fa fa-shopping-cart"></i>
-                        <span>E-commerce</span>
+                        <span>Home</span>
                         </a>
                         <div className="sidebar-submenu">
                         <ul>
@@ -54,24 +61,12 @@ export default function sideBar() {
                     <li className="sidebar-dropdown">
                         <a href="#">
                         <i className="far fa-gem"></i>
-                        <span>Components</span>
+                        <span>Utilisateurs</span>
                         </a>
                         <div className="sidebar-submenu">
                         <ul>
                             <li>
                             <a href="#">General</a>
-                            </li>
-                            <li>
-                            <a href="#">Panels</a>
-                            </li>
-                            <li>
-                            <a href="#">Tables</a>
-                            </li>
-                            <li>
-                            <a href="#">Icons</a>
-                            </li>
-                            <li>
-                            <a href="#">Forms</a>
                             </li>
                         </ul>
                         </div>
@@ -79,21 +74,12 @@ export default function sideBar() {
                     <li className="sidebar-dropdown">
                         <a href="#">
                         <i className="fa fa-chart-line"></i>
-                        <span>Charts</span>
+                        <span>Thèmes articles</span>
                         </a>
                         <div className="sidebar-submenu">
                         <ul>
                             <li>
                             <a href="#">Pie chart</a>
-                            </li>
-                            <li>
-                            <a href="#">Line chart</a>
-                            </li>
-                            <li>
-                            <a href="#">Bar chart</a>
-                            </li>
-                            <li>
-                            <a href="#">Histogram</a>
                             </li>
                         </ul>
                         </div>
@@ -101,15 +87,12 @@ export default function sideBar() {
                     <li className="sidebar-dropdown">
                         <a href="#">
                         <i className="fa fa-globe"></i>
-                        <span>Maps</span>
+                        <span>Articles</span>
                         </a>
                         <div className="sidebar-submenu">
                         <ul>
                             <li>
                             <a href="#">Google maps</a>
-                            </li>
-                            <li>
-                            <a href="#">Open street map</a>
                             </li>
                         </ul>
                         </div>
@@ -117,19 +100,43 @@ export default function sideBar() {
                     <li>
                         <a href="#">
                         <i className="fa fa-book"></i>
-                        <span>Documentation</span>
+                        <span>Trading & Crypto</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                        <i className="fa fa-book"></i>
+                        <span>Bot Trading</span>
                         </a>
                     </li>
                     <li>
                         <a href="#">
                         <i className="fa fa-calendar"></i>
-                        <span>Calendar</span>
+                        <span>Crypto Wallet</span>
                         </a>
                     </li>
                     <li>
                         <a href="#">
                         <i className="fa fa-folder"></i>
-                        <span>Examples</span>
+                        <span>Broker</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                        <i className="fa fa-folder"></i>
+                        <span>Pays</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                        <i className="fa fa-folder"></i>
+                        <span>Abonnement</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                        <i className="fa fa-folder"></i>
+                        <span>Settings</span>
                         </a>
                     </li>
                     </ul>
--- apps/dashboard/pages/themes-articles-creationNew.tsx
+++ apps/dashboard/pages/themes-articles-creationNew.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-//import Topbar from '../components/topbar';
+import Topbar from '../components/topbarNew';
 
 import style from '../styles/main.module.scss';
-import Sidebar from '../components/sidebar';
+import Sidebar from '../components/sidebarNew';
 
 export default function ThemesArticlesCreation() {
     return (
         <div>
+            <Topbar />
             <input type="hidden" id="anPageName" name="page" value="themes-articles-creation" />
             <Sidebar />
         </div>
