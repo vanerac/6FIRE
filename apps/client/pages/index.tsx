bf6a8dbbe565f100222738ec7815bda247799944
Zihad Hosan <zihadhossain88@hotmail.com>
2022-05-04 00:10:12 +0600

    index video block scroll

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,6 +1,18 @@
 import type { NextPage } from 'next';
 import Footer from './components/footer';
 import Head from 'next/head';
+import $ from 'jquery';
+
+/* scroll off */
+
+
+if (typeof window !== 'undefined') {
+    $("#video_trigger").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#video_wrapper").offset().top
+        }, 1000);
+    });
+}
 
 const HomePage: NextPage = ($props: any) => {
     return (
@@ -27,7 +39,7 @@ const HomePage: NextPage = ($props: any) => {
                             </div>
                         </a>
                     </div>
-                    <div className="mb-equal">
+                    <div className="mb-equal position-bottom" id='video_trigger'>
                         <p className="big-text">Visionner la vidéo obligatoire pour comprendre le club privé 6FIRE</p>
                         <div className="scroll-icon">
                             <img src="img/icon-material-keyboard-arrow-down@1x.png" alt="" />
@@ -38,7 +50,7 @@ const HomePage: NextPage = ($props: any) => {
             {/* Index header block END */}
 
             {/* Index Photo block */}
-            <div className="index-photo-block">
+            <div className="index-photo-block" id='video_wrapper'>
                 <div className="contanier">
                     <img src="img/cute-cat.jpg" alt="" />
                 </div>
