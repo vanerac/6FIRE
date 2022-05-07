96c7841cd7b87782d72f716649665a676c15588c
Zihad Hosan <zihadhossain88@hotmail.com>
2022-05-07 16:07:20 +0600

    /abonnement-creer-un-abonnement  update

--- apps/dashboard/pages/abonnement-creer-un-abonnement.tsx
+++ apps/dashboard/pages/abonnement-creer-un-abonnement.tsx
@@ -61,9 +61,154 @@ export default function CreerUnAbonnement() {
     };
 
     return (
-        <>
+        <div>
             <Topbar />
-            <SideBar />
-        </>
+            <input type="hidden" id="anPageName" name="page" value="themes-articles-creation" />
+
+            {/* Commone header wrapper */}
+
+            <div className="inner-page-setup">
+                <SideBar />
+
+                <div className="inner-wrapper">
+                    <div className="header">
+                        <h2 className="title">Gestion abonnements </h2>
+                    </div>
+
+                    <div className="table-header">
+                        <div>
+                                <button className="bg_green">
+                                    Creer un abonnement
+                                </button>
+                        </div>
+                    </div>
+                    <div className="table-wrapper">
+                        <div className="table-title bg_blue">
+                            <span>Information</span>
+                        </div>
+
+                        {/* table content */}
+                        <div className="table-content">
+                            <div className="row-1 inline-flex">
+                                <div className="single-item mr-30">
+                                    <label className="small_title" htmlFor="">
+                                        Nom 
+                                    </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Novice"
+                                    />
+                                </div>
+                                <div className="single-item mr-30">
+                                    <label className="small_title" htmlFor="">
+                                        Prix
+                                    </label>
+                                    <input
+                                        type="text"
+                                        placeholder="49.99 &euro;"
+                                    />
+                                </div>
+                                <div className="single-item mr-30">
+                                    <label className="small_title" htmlFor="">
+                                        Duree
+                                    </label>
+                                    <input
+                                        type="text"
+                                        placeholder="30"
+                                    />
+                                </div>
+                                <div className="single-item">
+                                    <label className="small_title" htmlFor="">
+                                        Type
+                                    </label>
+                                    <select
+                                        name=""
+                                        id=""
+                                    >
+                                        <option value="">
+                                            Mensuel
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className="row-1 inline-flex">
+                                <div className="single-item mr-30">
+                                    <div className="icon-box">
+                                        <input
+                                            type="checkbox"
+                                            className="checkBox-item"
+                                            name="Select file"
+                                        />
+                                        
+                                        <label htmlFor="test">Best seller</label>
+                                    </div>
+                                </div>
+                                <div className="single-item mr-30">
+                                    <div className="icon-box">
+                                        <input
+                                            type="checkbox"
+                                            className="checkBox-item"
+                                            name="Select file"
+                                        />
+                                        
+                                        <label htmlFor="test">Visible</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="table-wrapper">
+                        <div className="table-title bg_blue">
+                            <span>Plateforme de paiement</span>
+                        </div>
+
+                        {/* table content */}
+                        <div className="table-content">
+                            <div className="row-1 inline-flex">
+                                
+                                <div className="single-item">
+                                    <label className="small_title" htmlFor="">
+                                    Plateforme de paiement
+                                    </label>
+                                    <select
+                                        name=""
+                                        id=""
+                                    >
+                                        <option value="">
+                                            Plateforme
+                                        </option>
+                                        <option value="">
+                                            Plateforme de
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="table-wrapper">
+                        <div className="table-title bg_blue">
+                            <span>Lignes descriptives</span>
+                        </div>
+
+                        {/* table content */}
+                        <div className="table-content">
+                            <div className="row-1 inline-flex">
+                                
+                                <div className="single-item">
+                                    <input
+                                        type="text"
+                                        placeholder="Lignes 1"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     );
 }
