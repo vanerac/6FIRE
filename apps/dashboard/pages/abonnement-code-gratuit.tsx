f33c791e1ef9ad7a4ddd002161326a1831db024c
Zihad Hosan <zihadhossain88@hotmail.com>
2022-05-10 12:39:47 +0600

    /abonnement-code-gratuit style and markup

--- apps/dashboard/pages/abonnement-code-gratuit.tsx
+++ apps/dashboard/pages/abonnement-code-gratuit.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Topbar from '../components/topbarNew';
 import getAPIClient from '@shared/tools/apiClient';
-import { Offer } from '@shared/services';
+import { Theme } from '@shared/services';
 import router from 'next/router';
 import { useCookies } from 'react-cookie';
+import Sidebar from '../components/sidebarNew';
+import DataTable from '../components/data-table';
+// import { Theme } from '@shared/services';
+// import apiClient from '@shared/tools/apiClient';
 
-export default function AbonnementCodeGratuit() {
+export default function ThemesArticles() {
+    // const { query } = useRouter();
     const [cookies] = useCookies(['API_TOKEN']);
     const apiClient = getAPIClient(cookies['API_TOKEN']);
 
     const [$loading, setLoading] = useState(true);
     const [$error, setError] = useState('');
-    const [$offers, setOffers] = useState<Offer[]>();
+    const [$themes, setThemes] = useState<Theme[]>([]);
 
     useEffect(() => {
         if (!cookies['API_TOKEN']) {
@@ -20,9 +25,9 @@ export default function AbonnementCodeGratuit() {
             return;
         }
 
-        apiClient.offers.getAllOffers().then(
+        apiClient.themes.getThemes().then(
             (res) => {
-                setOffers(res);
+                setThemes(res);
                 setLoading(false);
             },
             (error) => {
@@ -32,201 +37,118 @@ export default function AbonnementCodeGratuit() {
         );
     }, []);
 
+    const deleteTheme = async (themeId: number) => {
+        console.log('deleteTheme', themeId);
+        try {
+            await apiClient.themes.deleteTheme(themeId);
+
+            alert('Theme supprimé');
+            const newThemes = $themes.filter((theme) => theme.id !== themeId);
+
+            setThemes(newThemes);
+        } catch (e) {
+            console.log(e);
+            alert('Echec de suppression');
+        }
+    };
+
     return (
-        <>
-            <input type="hidden" id="anPageName" name="page" value="abonnement-code-gratuit" />
-            <div className="container-center-horizontal">
-                <div className="abonnement-code-gratuit screen">
-                    <div className="analytics-IJFojW poppins-semibold-semi-bold-mirage-24px">Code gratuit (unique)</div>
-                    <div className="sidebar-IJFojW">
-                        <div className="shape-6Ihqtr"></div>
-                        <div className="settings-6Ihqtr">
-                            <div className="name-pofret poppins-medium-mirage-14px">Settings</div>
-                            <div className="icons-navigation-icon-12-states-pofret">
-                                <img className="ic_setting-aDxxQx" src="img/ic-setting-11@1x.png" />
-                            </div>
-                        </div>
-                        <div className="broker-6Ihqtr">
-                            <div className="name-Ka6Udf poppins-medium-mirage-14px">Broker</div>
-                            <img className="font-awsome-handshake-Ka6Udf" src="img/fontawsome--handshake--10@1x.png" />
-                        </div>
-                        <div className="crypto-wallet-6Ihqtr">
-                            <div className="name-DdAIRU poppins-medium-mirage-14px">Crypto Wallet</div>
-                            <img className="font-awsome-bitcoin-DdAIRU" src="img/fontawsome--bitcoin--11@1x.png" />
-                        </div>
-                        <div className="trading-crypto-6Ihqtr">
-                            <div className="name-PYjR5v poppins-medium-mirage-14px">Trading &amp; Crypto</div>
-                            <img className="font-awsome-pen-PYjR5v" src="img/fontawsome--pen--10@1x.png" />
-                        </div>
-                        <div className="thmes-articles-6Ihqtr">
-                            <div className="name-b6DEeK poppins-medium-mirage-14px">Thèmes articles</div>
-                            <div className="icons-navigation-icon-12-states-b6DEeK">
-                                <img
-                                    className="font-awsome-bookmark-IPKJmT"
-                                    src="img/fontawsome--bookmark--10@1x.png"
-                                />
-                            </div>
-                        </div>
-                        <div className="customers-6Ihqtr">
-                            <div className="name-XvcvVp poppins-medium-mirage-14px">Utilisateurs</div>
-                            <div className="icons-navigation-icon-12-states-XvcvVp">
-                                <img className="ic_users-xCFany" src="img/ic-users-11@1x.png" />
-                            </div>
-                        </div>
-                        <a href="home.html">
-                            <div className="home-6Ihqtr">
-                                <div className="name-JNl6FH poppins-medium-mirage-14px">Home</div>
-                                <div className="icons-navigation-icon-12-states-JNl6FH">
-                                    <img className="ic_home-NM2Tsa" src="img/ic-home-10@1x.png" />
-                                </div>
-                            </div>
-                        </a>
-                        <div className="articles-6Ihqtr">
-                            <div className="name-Vg1fvR poppins-medium-mirage-14px">Articles</div>
-                            <div className="icons-navigation-icon-12-states-Vg1fvR">
-                                <img className="ic_invoice-xBogVX" src="img/ic-invoice-10@1x.png" />
-                            </div>
-                            <img
-                                className="icon-ionic-ios-arrow-down-Vg1fvR"
-                                src="img/icon-ionic-ios-arrow-down-10@1x.png"
-                            />
-                        </div>
-                        <div className="bot-trading-6Ihqtr">
-                            <div className="name-OTIEIJ poppins-medium-mirage-14px">Bot Trading</div>
-                            <img className="font-awsome-robot-OTIEIJ" src="img/fontawsome--robot--10@1x.png" />
-                        </div>
-                        <div className="pays-6Ihqtr">
-                            <div className="name-g8qa4E poppins-medium-mirage-14px">Pays</div>
-                            <img
-                                className="font-awsome-globe-europe-g8qa4E"
-                                src="img/fontawsome--globe-europe--10@1x.png"
-                            />
-                        </div>
-                        <div className="abonnement-6Ihqtr">
-                            <img className="frame-pxBTHT" src="img/frame-37@1x.png" />
-                            <div className="line-pxBTHT"></div>
-                            <div className="name-pxBTHT poppins-medium-blue-ribbon-14px">Abonnement</div>
-                            <img className="icon-material-payment-pxBTHT" src="img/icon-material-payment-15@1x.png" />
-                            <a href="abonnement-gestion-abonnements.tsx">
-                                <div className="gestion-abonnements-pxBTHT">
-                                    <div className="frame-gzQDxO"></div>
-                                    <div className="name-gzQDxO poppins-medium-blue-ribbon-14px">
-                                        Gestion Abonnements
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="abonnement-code-promo.tsx">
-                                <div className="code-promo-pxBTHT">
-                                    <div className="frame-yaS4ec"></div>
-                                    <div className="name-yaS4ec poppins-medium-blue-ribbon-14px">Code promo</div>
-                                </div>
-                            </a>
-                            <a href="deprecated/abonnement-essai-gratuit.tsx">
-                                <div className="essai-gratuit-pxBTHT">
-                                    <img className="frame-DyYjTG" src="img/frame-11@1x.png" />
-                                    <div className="name-DyYjTG poppins-medium-blue-ribbon-14px">Essai gratuit</div>
-                                </div>
-                            </a>
-                            <div className="code-gratuit-pxBTHT">
-                                <img className="frame-YoWR5G" src="img/frame-11@1x.png" />
-                                <div className="name-YoWR5G poppins-medium-blue-ribbon-14px">Code gratuit</div>
-                            </div>
-                            <img
-                                className="icon-ionic-ios-arrow-down-pxBTHT"
-                                src="img/icon-ionic-ios-arrow-down-10@1x.png"
-                            />
-                        </div>
+        <div>
+            <Topbar />
+            <div className="inner-page-setup">
+                <Sidebar />
+                <div className="inner-wrapper">
+
+                    <div className="header">
+                        <h2 className="title">Code gratuit (unique)</h2>
+                        {/*<button className="" id="export_data">*/}
+                        {/*    <i className="fa fa-upload" /> <span>Export CSV</span>*/}
+                        {/*</button>*/}
                     </div>
-                    <Topbar />
-                    <div className="informations-IJFojW">
-                        <div className="rectangle-632-xQSJNS"></div>
-                        <div className="titre-xQSJNS">
-                            <div className="rectangle-633-xTUV77"></div>
-                            <div className="analytics-xTUV77 poppins-semibold-semi-bold-white-14px">
-                                Phrase à copier
-                            </div>
+
+                    <div className="table-wrapper">
+                        <div className="table-title bg_blue">
+                            <span>Phrase à copier</span>
                         </div>
-                        <div className="filtre-xQSJNS"></div>
-                        <div className="abonnement-xQSJNS">
-                            <img className="ligne-6-S5iyHv" src="img/ligne-6-67@1x.png" />
-                            <div className="ton-code-promo-code-S5iyHv poppins-normal-tundora-12px">
-                                Ton code promo : {'{'}code{'}'} à rentrer après avoir sélectionner l’offre intermédiaire
-                                sur le site
-                            </div>
-                            <div className="phrase-S5iyHv lato-normal-comet-10px">Phrase</div>
-                        </div>
-                    </div>
-                    <div className="grid-table-IJFojW">
-                        <div className="groupe-455-SnNlAD">
-                            <div className="rectangle-616-uggW2n"></div>
-                            <div className="rectangle-619-uggW2n"></div>
-                            <div className="rectangle-621-uggW2n"></div>
-                            <div className="rectangle-622-uggW2n"></div>
-                            <div className="rectangle-623-uggW2n"></div>
-                            <div className="rectangle-624-uggW2n"></div>
-                        </div>
-                        <div className="grid-table-SnNlAD">
-                            <div className="name-C1Jbgp poppins-medium-mirage-12px">Code unique</div>
-                            <div className="name-lA1zQM poppins-medium-mirage-12px">Campagne</div>
-                            <div className="name-CIhZxB poppins-medium-mirage-12px">Création</div>
-                            <div className="name-ZgdZZz poppins-medium-mirage-12px">Utiliser</div>
-                            <div className="name-IfuI7W poppins-medium-mirage-12px">Action</div>
-                            <div className="name-lIucNx poppins-medium-mirage-12px">Utiliser par</div>
-                        </div>
-                        <div className="exemple-row-SnNlAD">
-                            <div className="row-1-foYVaV">
-                                <div className="rectangle-617-YLkFmZ"></div>
-                                <div className="name-YLkFmZ poppins-medium-tundora-10px">XOvE39b</div>
-                                <div className="name-xaWnbc poppins-medium-tundora-10px">Broker (30j)</div>
-                                <div className="name-fwztxb poppins-medium-tundora-10px">22/03/22</div>
-                                <div className="membre-YLkFmZ">
-                                    <img
-                                        className="font-awsome-check-square-kW0kBh"
-                                        src="img/fontawsome--check-square--10@1x.png"
+                        <div className="table-content">
+                            <div className="row-1 inline-flex">
+                                <div className="single-item width-100">
+                                    <label className="small_title" htmlFor="">
+                                        Phrase
+                                    </label>
+                                    <input className='width-100'
+                                        type="text"
+                                        placeholder=""
                                     />
                                 </div>
-                                <img className="font-awsome-pen-YLkFmZ" src="img/fontawsome--pen--12@1x.png" />
-                                <img className="font-awsome-times-YLkFmZ" src="img/fontawsome--times--10@1x.png" />
-                                <div className="icon-feather-copy-YLkFmZ">
-                                    <img className="trac-997-Mxidp4" src="img/trac--997-1@1x.png" />
-                                    <img className="trac-998-Mxidp4" src="img/trac--998-1@1x.png" />
-                                </div>
-                                <div className="name-LUxa8K poppins-medium-tundora-10px">elodie@elodie.fr</div>
                             </div>
-                            <div className="row-2-foYVaV">
-                                <div className="rectangle-618-svZSSv"></div>
-                                <div className="name-svZSSv poppins-medium-tundora-10px">XOvE39b</div>
-                                <div className="name-wrnKVX poppins-medium-tundora-10px">Broker (30j)</div>
-                                <div className="name-MkcXpB poppins-medium-tundora-10px">22/03/22</div>
-                                <div className="membre-svZSSv">
-                                    <img
-                                        className="font-awsome-window-close-aW4paS"
-                                        src="img/fontawsome--window-close--12@1x.png"
-                                    />
-                                </div>
-                                <img className="font-awsome-pen-svZSSv" src="img/fontawsome--pen--12@1x.png" />
-                                <img className="font-awsome-times-svZSSv" src="img/fontawsome--times--10@1x.png" />
-                                <div className="icon-feather-copy-svZSSv">
-                                    <img className="trac-997-FtrAfj" src="img/trac--997-1@1x.png" />
-                                    <img className="trac-998-FtrAfj" src="img/trac--998-1@1x.png" />
-                                </div>
+                            </div>
+                    </div>
+
+                    <div className="table-header table-col-2">
+                        <div className="left-col-search">
+                            <div className="search-text">
+                                <input type="text" placeholder='Rechercher...' />
+                            </div>
+                            <div className="selector">
+                                <select name="" id="">
+                                    <option value="0">select</option>
+                                    <option value="0">Utiliser</option>
+                                </select>
                             </div>
                         </div>
                     </div>
-                    <div className="search-IJFojW">
-                        <div className="rectangle-625-gvtN8P"></div>
-                        <div className="name-gvtN8P poppins-medium-mirage-10px">Rechercher…</div>
-                    </div>
-                    <div className="filtre-IJFojW">
-                        <div className="rectangle-625-p335g5"></div>
-                        <div className="name-p335g5 poppins-medium-mirage-10px">Utiliser</div>
-                        <img
-                            className="font-awsome-chevron-down-p335g5"
-                            src="img/fontawsome--chevron-down--10@1x.png"
-                        />
-                    </div>
+                    <div className="table-wrapper">
+                {/* table content */}
+                <div className="table-content mt-0">
+                    <table>
+                        <thead>
+                            <tr>
+                                <td className="">Code unique</td>
+                                <td className="">Campagne</td>
+                                <td className="">Création</td>
+                                <td className="width-15">Utiliser</td>
+                                <td className="width-15">Utiliser par</td>
+                                <td className="text-right">Action</td>
+                            </tr>
+                        </thead>
+
+                        {/* single row */}
+                        <tr>
+                           <td>XOvE39b</td>
+                           <td>Broker (30j)</td>
+                           <td>22/03/22</td>
+                           <td className="width-15">
+                               <i className="far fa-check-square color-light-green"></i>
+                           </td>
+                           <td>elodie@elodie.fr</td>
+                           <td className="text-right ">
+                               <i className="far fa-clone color-dard-blue mr-30"></i>
+                               <i className="fa fa-edit color-dard-blue mr-30"></i>
+                               <i className="fas fa-times color-red"></i>
+                           </td>
+                        </tr>
+                        {/* single row */}
+                        <tr>
+                           <td>XOvE39b</td>
+                           <td>Broker (30j)</td>
+                           <td>22/03/22</td>
+                           <td className="width-15">
+                               <i className="far fa-window-close color-red"></i>
+                           </td>
+                           <td>elodie@elodie.fr</td>
+                           <td className="text-right ">
+                               <i className="far fa-clone color-dard-blue mr-30"></i>
+                               <i className="fa fa-edit color-dard-blue mr-30"></i>
+                               <i className="fas fa-times color-red"></i>
+                           </td>
+                        </tr>
+
+                    </table>
                 </div>
             </div>
-        </>
+                </div>
+            </div>
+        </div>
     );
 }
